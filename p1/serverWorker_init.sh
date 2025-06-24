85a1b72f09411e3ad3bdee1e237d7520f821a75c
ecnaraga <garance.lambey@gmail.com>
2025-06-24 16:31:12 +0200

    corr mess error dpkg + Tell k3s to use NODE_IP istead of default ip

--- p1/serverWorker_init.sh
+++ p1/serverWorker_init.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo "Hello worker"
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl -y
-sudo apt install ufw -y
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get upgrade -y
+apt-get install curl -y
+apt-get install ufw -y
 
 # Disable FireWall
 ufw disable
 
 # Recover the token fron the shared folder
 TOKEN=$(cat /vagrant/token)
+NODE_IP=192.168.56.111
 # K3S_URL => Indicates the ip of the server node and the gate to connect
 # K3S_TOKEN => Allow the worker to authentificate
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${TOKEN} sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${TOKEN} INSTALL_K3S_EXEC="--node-ip=${NODE_IP}" sh -
--- p1/server_init.sh
+++ p1/server_init.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-echo "Hello server"
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl -y
-sudo apt install ufw -y
-# sudo apt install net-tools
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get upgrade -y
+apt-get install curl -y
+apt-get install ufw -y
 
 # Disable FireWall
-sudo ufw disable
+ufw disable
 
 # Get installation's script of k3s | Exec the script with sh in order to install k3s in controller mode
 # -s : silent
 # -f : If http error -> fail
 # -L : follow redirections
-curl -sfL https://get.k3s.io | sh -
+NODE_IP=192.168.56.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${NODE_IP}" sh -
 
-sudo mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+mkdir -p /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+chown -R vagrant:vagrant /home/vagrant/.kube/config
 
 # To put the token in the shared vagrant folder in order to allow the worker to use it and connect to the cluster
 TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+echo "TEST 9"
 echo ${TOKEN} > /vagrant/token
