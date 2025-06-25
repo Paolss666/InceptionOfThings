ac212addc43abe04e9cae3fb7fac63808ab96f14
ecnaraga <garance.lambey@gmail.com>
2025-06-25 16:35:47 +0200

    corr bug dpkg + MAJ Internal IP adress + alias k

--- p2/server_init.sh
+++ p2/server_init.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-echo "Hello server"
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl -y
-sudo apt install ufw -y
-# sudo apt install net-tools
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get upgrade -y
+apt-get install curl -y
+apt-get install ufw -y
 
 # Disable FireWall
-sudo ufw disable
+ufw disable
 
 # Get installation's script of k3s | Exec the script with sh in order to install k3s in controller mode
 # -s : silent
 # -f : If http error -> fail
 # -L : follow redirections
-curl -sfL https://get.k3s.io | sh -
+# INSTALL_K3S_EXEC="--node-ip=${NODE_IP}" => Prefix environment variable that tell K3S to use Node_IP instead of default one as Internal_IP  
+NODE_IP=192.168.56.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=${NODE_IP}" sh -
 
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
@@ -21,5 +22,11 @@ sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
 
 # To put the token in the shared vagrant folder in order to allow the worker to use it and connect to the cluster
 TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
-# echo ${TOKEN} > /vagrant/token
 echo ${TOKEN} > /src/token
+# To create k alias
+echo 'alias k="sudo kubectl"' > /home/vagrant/.bashrc
+# Create deployment, services and ingress for app-one app-two and app-three  
+sudo kubectl apply -f /src/appX.com/app-one.yml
+sudo kubectl apply -f /src/appX.com/app-two.yml
+sudo kubectl apply -f /src/appX.com/app-three.yml
+sudo kubectl apply -f /src/appX.com/ingress-conf.yml
